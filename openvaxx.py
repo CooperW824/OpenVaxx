8199fec1bce2db0e6da3b646b1bc821a86e47074
Cooper Werner <cooperwerner824@gmail.com>
2021-09-10 14:12:17 -0400

    gui: first ui elements

--- openvaxx.py
+++ openvaxx.py
@@ -1,4 +1,5 @@
-from functools import WRAPPER_ASSIGNMENTS
+
+from PySimpleGUI.PySimpleGUI import Column, VSeparator
 from OpenVaxxDB import business as ovb
 from OpenVaxxDB import distributor as ovd
 from OpenVaxxDB import recipient as ovr
@@ -10,12 +11,27 @@ bgColor1 = '#ffffff'
 bgColor2 = '#0e1778'
 textColor1 = '#ffffff'
 textColor2 = '#0e1778'
+buttonBgColor = "#030947"
 notVaxxColor = '#e3210b'
 warningColor = '#ff9d00'
 fullVaxxColor = '#00b315'
 
+
 # -- Window Layouts -- 
-aboutPage = [[]]
+aboutPage = [[
+    sg.Column([[
+        sg.Column(
+        [[sg.Text("OpenVaxx Vaccine Passport Demo", (45, 1), text_color=textColor1, font="Arial", background_color=bgColor2)]],
+         background_color=bgColor2
+         ),
+    sg.Column(
+        [[sg.Button("Login", font="Arial", button_color=buttonBgColor),
+        sg.Button("Signup", font="Arial", button_color=buttonBgColor )]],
+        background_color=bgColor2
+        )
+        ]], background_color=bgColor2, justification="center")
+    
+        ]]
 recipentLogin = [[]]
 distributorLogin = [[]]
 businessLogin = [[]]
@@ -30,7 +46,7 @@ settingsPage = [[]]
 # -- New Window Functions -- 
 
 def open_login_window():
-    window = sg.Window("Login into Your Account", loginPage, modal=True)
+    window = sg.Window("Login into Your OpenVaxx Account", loginPage, modal=True)
     while True:
         event, values = window.read()
         if event == "Exit" or event == sg.WIN_CLOSED:
@@ -39,11 +55,13 @@ def open_login_window():
     window.close()
 
 def open_recipient_page(username):
-    window = sg.Window("Welcome " + username, recipientPage)
+    window = sg.Window("OpenVaxx, Welcome " + username, recipientPage)
     while True:
         event, values = window.read()
         if event == "Exit" or event == sg.WIN_CLOSED:
             break
+    
+    window.close()
 
 def open_distributor_main_page(distributor):
     window = sg.Window(distributor, loginPage, modal=True)
@@ -52,20 +70,24 @@ def open_distributor_main_page(distributor):
         if event == "Exit" or event == sg.WIN_CLOSED:
             break
 
+    window.close()
+
 def open_distributor_input_page(distributor):
     window = sg.Window(distributor + "Vaccine Info Input", loginPage, modal=True)
     while True:
         event, values = window.read()
         if event == "Exit" or event == sg.WIN_CLOSED:
             break
+    
+    window.close()
 
 
 # Create the Window
-aboutWindow = sg.Window('Window Title', aboutPage, size=(200,200))
+aboutWindow = sg.Window('OpenVaxx', aboutPage, size=(750,400), background_color="#ffffff")
 # Event Loop to process "events" and get the "values" of the inputs
 while True:
     event, values = aboutWindow.read()
     if event == sg.WIN_CLOSED or event == 'Cancel': # if user closes window or clicks cancel
-        break 
+        break
 
 aboutWindow.close()
